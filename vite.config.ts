081022b937ffd29b97b8281fe40a6e1309a29d57
Andrés Meléndez <codeslator@gmail.com>
2023-07-30 00:26:46 -0400

    Change path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://codeslator.github.io/',
+  base: 'https://codeslator.github.io/gh-react-vite/',
   plugins: [react()],
 })
